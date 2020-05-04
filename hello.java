70be186e2403bc2bc684f5a35a9aef6d0810aaa5
Foday Sanyang <phonum5@gmail.com>
2020-05-04 13:02:35 +0000

    Add loop

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Pluralsight");
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("Am learning Java");
+		}
 	}
 }
